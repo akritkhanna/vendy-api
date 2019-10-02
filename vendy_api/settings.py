21c66bc15769426e9f36eb0990c068b2c5e7bc86
Akrit Khanna <akritkhanna@users.noreply.github.com>
2019-10-02 14:40:29 +0530

    Pipeline test using heroku(#1) -added static

--- vendy_api/settings.py
+++ vendy_api/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vendy_api.urls'
@@ -151,3 +152,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
