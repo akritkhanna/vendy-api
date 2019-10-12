3ad5fb6daafb6493392853d44e0eb266364f29dc
Akrit Khanna <34682943+akritkhanna@users.noreply.github.com>
2019-10-12 06:45:07 +0530

    User model Updated

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Categories(models.Model):
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)s
     sub_types = models.ManyToManyField('category.SubCategories', default=None, blank=True, null=True)
 
     def __str__(self):
