ecdf40ed6d57fab38e72aedb5212cd6f4d9d87d3
Akrit Khanna <34682943+akritkhanna@users.noreply.github.com>
2020-04-08 22:24:41 +0530

    Update settings.py

--- vendy_api/settings.py
+++ vendy_api/settings.py
@@ -98,7 +98,12 @@ WSGI_APPLICATION = 'vendy_api.wsgi.application'
 
 
 # MEDIA STORAGE(CURRENTLY AWS S3)
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = #'removed'
+AWS_SECRET_ACCESS_KEY = #'removed'
+AWS_STORAGE_BUCKET_NAME = #'removed'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Database
