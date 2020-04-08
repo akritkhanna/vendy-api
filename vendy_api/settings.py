684a481b89cc8fe60300afcd30a67ec01f54ed55
Akrit Khanna <34682943+akritkhanna@users.noreply.github.com>
2020-04-08 22:17:36 +0530

    security measures

--- vendy_api/settings.py
+++ vendy_api/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'vendy_api.wsgi.application'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIA45MLJQQQBPBDP65N'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'vendy-storage'
+AWS_ACCESS_KEY_ID = #removed due to security issues
+AWS_SECRET_ACCESS_KEY = #removed due to security issues
+AWS_STORAGE_BUCKET_NAME = #removed due to security issues
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
