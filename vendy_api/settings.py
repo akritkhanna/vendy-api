ddfc783e81c5b52c2aa1422cfaf5958e65fdcb71
Akrit Khanna <akritkhanna@users.noreply.github.com>
2020-04-21 22:28:08 +0530

    temporary host

--- vendy_api/settings.py
+++ vendy_api/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'vendy_api.wsgi.application'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIA45MLJQQQBPBDP65N'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'vendy-storage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
@@ -114,11 +114,11 @@ AWS_DEFAULT_ACL = None
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cszyjhkt',
-        'USER': 'cszyjhkt',
-        'PASSWORD': 'lgytDf9-rTrPk6qyjRTqHpmvcnrGA5Qz',
-        'HOST': 'salt.db.elephantsql.com',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PWD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
